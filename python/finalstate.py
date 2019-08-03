94411b1df79ec9eeb0dd616b5aee2efbd912c6a0
Kevin Zheng <coffeevector@gmail.com>
2019-08-03 00:52:06 -0700

    Removed algorithm section because the algorithm has a mistake

--- python/finalstate.py
+++ python/finalstate.py
@@ -1,21 +1,19 @@
 #!/usr/bin/python
 import matplotlib.pyplot as plt
-
-cap = 4 - 1
-bot = 2 - 1
-
+cap = 3
+bot = 1
 capcount = 0
 botcount = 0
 
 def final(a, b):
     global capcount
     global botcount
-    if a / cap > 0:
-        capcount = capcount + (a / cap)
-        return final(a % cap , b)
-    elif b / bot > 0:
-        botcount = botcount + (b / bot)
-        return final(a, b % bot)
+    if a >= cap:
+        capcount = capcount + 1
+        return final(a - 3, b + 1)
+    elif b >= bot:
+        botcount = botcount + 1
+        return final(a + 1, b - 1)
     return (a, b)
 
 finalVectors = [final(i, i) for i in range(30)]
@@ -26,9 +24,11 @@ bots = [j for i,j in finalVectors]
 x = range(30)
 
 plt.plot(x, bots, label="Bottles left over")
+plt.xticks(x)
 plt.legend()
 plt.savefig("bots.png")
 plt.clf()
 plt.plot(x, caps, label="Caps left over")
+plt.xticks(x)
 plt.legend()
 plt.savefig("caps.png")
